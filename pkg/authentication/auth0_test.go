8816332b541ee016f6447bbd80de872494ad31e0
Marcos Huck <marcos@huck.com.ar>
2023-04-28 15:07:10 -0300

    Rename suite

--- pkg/authentication/auth0_test.go
+++ pkg/authentication/auth0_test.go
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-type tokenTestSuite struct {
+type auth0TestSuite struct {
 	suite.Suite
 	authentication Authentication
 	token          *jwt.Token
@@ -20,11 +20,11 @@ type tokenTestSuite struct {
 	privateKey     *rsa.PrivateKey
 }
 
-func TestTokenTestSuite(t *testing.T) {
-	suite.Run(t, new(tokenTestSuite))
+func TestAuth0TestSuite(t *testing.T) {
+	suite.Run(t, new(auth0TestSuite))
 }
 
-func (suite *tokenTestSuite) SetupSuite() {
+func (suite *auth0TestSuite) SetupSuite() {
 	var err error
 	suite.Require().NoError(err)
 	suite.token = jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims(map[string]interface{}{
@@ -46,11 +46,11 @@ func (suite *tokenTestSuite) SetupSuite() {
 	suite.authentication = NewAuth0(suite.publicKey)
 }
 
-func (suite *tokenTestSuite) SetupTest() {
+func (suite *auth0TestSuite) SetupTest() {
 
 }
 
-func (suite *tokenTestSuite) TestVerifyCredentials_InvalidToken() {
+func (suite *auth0TestSuite) TestVerifyCredentials_InvalidToken() {
 	ctx := context.Background()
 
 	suite.Assert().Error(suite.authentication.VerifyJWT(ctx, ""))
@@ -64,7 +64,7 @@ func (suite *tokenTestSuite) TestVerifyCredentials_InvalidToken() {
 	suite.Assert().Error(suite.authentication.VerifyJWT(ctx, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL215LWRvbWFpbi5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8MTIzNDU2IiwiYXVkIjpbImh0dHBzOi8vZXhhbXBsZS5jb20vaGVhbHRoLWFwaSIsImh0dHBzOi8vbXktZG9tYWluLmF1dGgwLmNvbS91c2VyaW5mbyJdLCJhenAiOiJteV9jbGllbnRfaWQiLCJleHAiOjEzMTEyODE5NzAsImlhdCI6MTMxMTI4MDk3MCwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSByZWFkOnBhdGllbnRzIHJlYWQ6YWRtaW4ifQ."))
 }
 
-func (suite *tokenTestSuite) TestVerifyCredentials_Success() {
+func (suite *auth0TestSuite) TestVerifyCredentials_Success() {
 	ctx := context.Background()
 
 	signedToken, err := suite.token.SignedString(suite.privateKey)
@@ -73,10 +73,10 @@ func (suite *tokenTestSuite) TestVerifyCredentials_Success() {
 	suite.Assert().NoError(suite.authentication.VerifyJWT(ctx, signedToken))
 }
 
-func (suite *tokenTestSuite) TearDownTest() {
+func (suite *auth0TestSuite) TearDownTest() {
 
 }
 
-func (suite *tokenTestSuite) TearDownSuite() {
+func (suite *auth0TestSuite) TearDownSuite() {
 
 }
