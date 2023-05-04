baba13dcebe671e311062ac2620c3c2cd21eea15
Marcos Huck <marcos@huck.com.ar>
2023-05-04 12:45:00 -0300

    Improve middleware generation by using Extractors and TokenAuthentication

--- pkg/authentication/authentication.go
+++ pkg/authentication/authentication.go
@@ -4,6 +4,9 @@ import (
 	"context"
 )
 
+// TokenAuthentication is the signature that a function should fulfill in order to verify an access token.
+type TokenAuthentication func(context.Context, string) error
+
 // Authentication contains a set of methods to authenticate users through different authentication providers such as
 // Auth0, Google Identity Platform, and such.
 type Authentication interface {
