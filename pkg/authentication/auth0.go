64d2a991dceeb8a390b3a5fa32ad44bc2bd604af
Marcos Huck <marcos@huck.com.ar>
2023-05-08 11:32:38 -0300

    Extract validateJWT

--- pkg/authentication/auth0.go
+++ pkg/authentication/auth0.go
@@ -3,9 +3,7 @@ package authentication
 import (
 	"context"
 	"errors"
-	"fmt"
 	"github.com/golang-jwt/jwt/v5"
-	"strings"
 )
 
 var (
@@ -23,7 +21,7 @@ type auth0 struct {
 
 // VerifyJWT verifies that the given token is a valid JWT and was correctly signed by Auth0.
 func (auth *auth0) VerifyJWT(ctx context.Context, token string) error {
-	if err := auth.validateJWT(token); err != nil {
+	if err := validateJWT(token); err != nil {
 		return err
 	}
 	parsedToken, err := jwt.Parse(token, auth.keyFunc)
@@ -32,30 +30,6 @@ func (auth *auth0) VerifyJWT(ctx context.Context, token string) error {
 	}
 	if !parsedToken.Valid {
 		return ErrTokenInvalid
-	}
-	return nil
-}
-
-// validateJWT validates that the given token is a valid JWT.
-func (auth *auth0) validateJWT(token string) error {
-	if len(token) == 0 {
-		return ErrTokenNotProvided
-	}
-	parts := strings.Split(token, ".")
-	if len(parts) != 3 {
-		return fmt.Errorf("%w: malformed", ErrTokenInvalid)
-	}
-	header := parts[0]
-	payload := parts[1]
-	sig := parts[2]
-	if len(header) == 0 {
-		return fmt.Errorf("%w: no jwt header", ErrTokenInvalid)
-	}
-	if len(payload) == 0 {
-		return fmt.Errorf("%w: no jwt payload", ErrTokenInvalid)
-	}
-	if len(sig) == 0 {
-		return fmt.Errorf("%w: no jwt signature", ErrTokenInvalid)
 	}
 	return nil
 }
